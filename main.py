980a836b1378bb22c77bec55c96d8ffb6e1e7a70
RaphaelMacedoB <faeldojo@gmail.com>
2023-07-19 17:32:32 -0300

    feat: normalizando cromas e salvando croma/rotulo

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import librosa
 import numpy as np
 
 file_path = 'musicas'
+output_path = 'dataset'
 
 folder = os.listdir(file_path)
 
@@ -21,17 +22,20 @@ def obter_tonalidade(nome_arquivo):
 
 def obter_cromas(files):
     for file in files:
-        caminho_arquivo = os.path.join('musicas',file)
+        caminho_arquivo = os.path.join(file_path, file)
         y, sr = librosa.load(caminho_arquivo)
         croma = librosa.feature.chroma_cqt(y=y, sr=sr)
-        cromas.append(croma)
+        croma_normalizado = librosa.util.normalize(croma)  # Normalização dos cromas
+        cromas.append(croma_normalizado)
         rotulos.append(obter_tonalidade(file))
+        print(caminho_arquivo,obter_tonalidade(file))
 
+# Extrair os cromas e rótulos
 obter_cromas(folder)
 
-i = 1
+# Salvar os dados em arquivos .npy com os respectivos rótulos
+for i, croma in enumerate(cromas):
+    np.save(os.path.join(output_path, f'{i+1}.npy'), croma)
+    np.save(os.path.join(output_path, f'{i+1}_label.npy'), rotulos[i])
 
-for croma in cromas:
-    np.save(f'dataset/{i}', croma)
-    i += 1
-    
+print('Dados salvos em', output_path)
